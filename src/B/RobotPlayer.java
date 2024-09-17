6b7f1e24ac7aed70c9741d6aa0d1a461067ebc1e
Dennis Drost <dennisrdrost@gmail.com>
2024-09-17 11:31:52 +0200

    decide logic

--- src/B/RobotPlayer.java
+++ src/B/RobotPlayer.java
@@ -27,16 +27,18 @@ public strictfp class RobotPlayer {
      */
     static final Random rng = new Random();
 
-    /** Array containing all the possible movement directions. */
+    /**
+     * Array containing all the possible movement directions.
+     */
     static final Direction[] directions = {
-        Direction.NORTH,
-        Direction.NORTHEAST,
-        Direction.EAST,
-        Direction.SOUTHEAST,
-        Direction.SOUTH,
-        Direction.SOUTHWEST,
-        Direction.WEST,
-        Direction.NORTHWEST,
+            Direction.NORTH,
+            Direction.NORTHEAST,
+            Direction.EAST,
+            Direction.SOUTHEAST,
+            Direction.SOUTH,
+            Direction.SOUTHWEST,
+            Direction.WEST,
+            Direction.NORTHWEST,
     };
 
     enum Strategy {
@@ -51,8 +53,8 @@ public strictfp class RobotPlayer {
      * run() is the method that is called when a robot is instantiated in the Battlecode world.
      * It is like the main function for your robot. If this method returns, the robot dies!
      *
-     * @param rc  The RobotController object. You use it to perform actions from this robot, and to get
-     *            information on its current status. Essentially your portal to interacting with the world.
+     * @param rc The RobotController object. You use it to perform actions from this robot, and to get
+     *           information on its current status. Essentially your portal to interacting with the world.
      **/
     @SuppressWarnings("unused")
     public static void run(RobotController rc) throws GameActionException {
@@ -75,10 +77,9 @@ public strictfp class RobotPlayer {
             try {
                 // Make sure you spawn your robot in before you attempt to take any actions!
                 // Robots not spawned in do not have vision of any tiles and cannot perform any actions.
-                if (!rc.isSpawned()){
+                if (!rc.isSpawned()) {
                     spawn(rc);
-                }
-                else{
+                } else {
                     decide(rc);
 
                     switch (strategy) {
@@ -122,7 +123,16 @@ public strictfp class RobotPlayer {
     }
 
     private static void decide(RobotController rc) throws GameActionException {
-        strategy = Strategy.RANDOM;
+        if (rc.getRoundNum() < GameConstants.SETUP_ROUNDS) {
+            strategy = Strategy.RANDOM;
+            return;
+        }
+
+        if (rc.hasFlag()) {
+            strategy = Strategy.GO_HOME;
+        } else {
+            strategy = Strategy.CAPTURE;
+        }
     }
 
     private static void spawn(RobotController rc) throws GameActionException {
@@ -136,10 +146,9 @@ public strictfp class RobotPlayer {
         // Move and attack randomly if no objective.
         Direction dir = directions[rng.nextInt(directions.length)];
         MapLocation nextLoc = rc.getLocation().add(dir);
-        if (rc.canMove(dir)){
+        if (rc.canMove(dir)) {
             rc.move(dir);
-        }
-        else if (rc.canAttack(nextLoc)){
+        } else if (rc.canAttack(nextLoc)) {
             rc.attack(nextLoc);
             System.out.println("Take that! Damaged an enemy that was in our way!");
         }
@@ -151,7 +160,7 @@ public strictfp class RobotPlayer {
     }
 
     private static void capture(RobotController rc) throws GameActionException {
-        if (rc.canPickupFlag(rc.getLocation())){
+        if (rc.canPickupFlag(rc.getLocation())) {
             rc.pickupFlag(rc.getLocation());
             rc.setIndicatorString("Holding a flag!");
         }
@@ -161,14 +170,14 @@ public strictfp class RobotPlayer {
         // If we are holding an enemy flag, singularly focus on moving towards
         // an ally spawn zone to capture it! We use the check roundNum >= SETUP_ROUNDS
         // to make sure setup phase has ended.
-        if (rc.hasFlag() && rc.getRoundNum() >= GameConstants.SETUP_ROUNDS){
+        if (rc.hasFlag() && rc.getRoundNum() >= GameConstants.SETUP_ROUNDS) {
             MapLocation[] spawnLocs = rc.getAllySpawnLocations();
             MapLocation firstLoc = spawnLocs[0];
             Direction dir = rc.getLocation().directionTo(firstLoc);
             if (rc.canMove(dir)) rc.move(dir);
             else {
                 Arrays.stream(directions)
-                        .filter(direction -> direction!=dir)
+                        .filter(direction -> direction != dir)
                         .filter(direction -> rc.canMove(direction))
                         .findFirst()
                         .ifPresent(direction -> {
@@ -182,19 +191,19 @@ public strictfp class RobotPlayer {
         }
     }
 
-    private static void updateEnemyRobots(RobotController rc) throws GameActionException{
+    private static void updateEnemyRobots(RobotController rc) throws GameActionException {
         // Sensing methods can be passed in a radius of -1 to automatically
         // use the largest possible value.
         RobotInfo[] enemyRobots = rc.senseNearbyRobots(-1, rc.getTeam().opponent());
-        if (enemyRobots.length != 0){
+        if (enemyRobots.length != 0) {
             rc.setIndicatorString("There are nearby enemy robots! Scary!");
             // Save an array of locations with enemy robots in them for future use.
             MapLocation[] enemyLocations = new MapLocation[enemyRobots.length];
-            for (int i = 0; i < enemyRobots.length; i++){
+            for (int i = 0; i < enemyRobots.length; i++) {
                 enemyLocations[i] = enemyRobots[i].getLocation();
             }
             // Let the rest of our team know how many enemy robots we see!
-            if (rc.canWriteSharedArray(0, enemyRobots.length)){
+            if (rc.canWriteSharedArray(0, enemyRobots.length)) {
                 rc.writeSharedArray(0, enemyRobots.length);
                 int numEnemies = rc.readSharedArray(0);
             }
